57f93ee92f3ce77ca98252a78e610ecb58108a25
kamil <kamil@KAMIL-PC>
2022-07-20 18:29:29 +0200

    Add project files.

---
+++ NozIWidelecScraper.cs
@@ -0,0 +1,51 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DinnerWebScraper
+{
+    class NozIWidelecScraper
+    {
+        private const string BaseUrl = "https://nozwideleccatering.pl/";
+
+        public HtmlWeb Web { get; private set; }
+        public HtmlDocument Doc { get; private set; }
+
+        public NozIWidelecScraper()
+        {
+            this.Web = new HtmlWeb();
+            this.Doc = Web.Load(BaseUrl);
+        }
+
+        public void GetAllDinnersForToday()
+        {
+            var date = GetDate();
+            var soup = GetSoup();
+            var dinners = GetDinners();
+
+            Console.WriteLine($"Dzień: {date}\nZupa: {soup}\nDrugie danie:\n{dinners}");
+        }
+
+        private string GetDate() => Doc.QuerySelector("#custom_post_widget-36")
+            .InnerText
+            .Replace("&nbsp;", "")
+            .Replace("\n", "");
+
+        private string GetSoup() => Doc.QuerySelector("#custom_post_widget-34")
+            .InnerText
+            .Split(' ')[0];
+
+        private string GetDinners()
+        {
+            var dinners = Doc.QuerySelector("#custom_post_widget-24")
+                .QuerySelectorAll("p")
+                .Where(p => char.IsDigit(char.Parse(p.InnerText.Substring(0, 1))))
+                .Select(n => n.InnerText);
+
+            return string.Join('\n', dinners);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace DinnerWebScraper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var scraper = new NozIWidelecScraper();
+            scraper.GetAllDinnersForToday();
+        }
+    }
+}
